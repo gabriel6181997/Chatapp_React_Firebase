16de99d16517d6b7068a01ae59150c33ecdbf665
“gabriel6181997” <gabriel6181997@gmail.com>
2021-02-10 21:52:55 +0800

    Use Grid

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { Avatar, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
+    margin: '26px',
   },
 });
 
-export function MessageInputField () {
+export function MessageInputField() {
   const classes = useStyles();
-  return <div className={classes.root}>MessageInputField</div>
+  return (
+  <div className={classes.root}>
+    <Grid container>
+      <Grid xs={1}>
+        <Avatar />
+      </Grid>
+      <Grid xs={10}>
+        入力
+      </Grid>
+      <Grid xs={1}>
+        ボタン
+      </Grid>
+    </Grid>
+  </div>
+  );
 };
-

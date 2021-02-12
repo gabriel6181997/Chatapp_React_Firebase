4d8915a81f3f34eef54c1fe6b8f9e7ab702bf82b
“gabriel6181997” <gabriel6181997@gmail.com>
2021-02-12 12:33:17 +0800

    Install Firebase

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,9 +1,9 @@
 import React, {useState} from "react";
 import { TextField } from "@material-ui/core";
+import { pushMessage } from '../firebase';
 
 export function MessageField ({ name, setText, text }) {
   const [isComposed, setIsComposed] = useState(false);
-  console.log({ text });
 
   return (
     <TextField
@@ -16,7 +16,7 @@ export function MessageField ({ name, setText, text }) {
         if (text === '') return;
 
         if(e.key === 'Enter'){
-          console.log('push message to firebase');
+          pushMessage({name:'Gabriel',text });
           setText('');
           e.preventDefault();
         }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+  import firebase from 'firebase';
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBCaXwCQc1sOjGzdrQ0r-Vql4uZ4ZmiUBc",
+    authDomain: "react-firebase-chatapp-1d670.firebaseapp.com",
+    projectId: "react-firebase-chatapp-1d670",
+    storageBucket: "react-firebase-chatapp-1d670.appspot.com",
+    messagingSenderId: "543470504702",
+    appId: "1:543470504702:web:5c3f0a813422384e3b73af"
+  };
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  const messageRef = database.ref('messages');
+
+  export const pushMessage = ({name, text}) => {
+   messageRef.push({name,text});
+  }

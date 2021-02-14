a62891f2065fc2b5402d619c2b280ba26a6da1e2
“gabriel6181997” <gabriel6181997@gmail.com>
2021-02-14 16:33:49 +0800

    Remove Unnecessary code

--- src/components/MessageSubmitButton.js
+++ src/components/MessageSubmitButton.js
@@ -2,7 +2,6 @@ import React from "react";
 import IconButton from '@material-ui/core/IconButton';
 import SendIcon from '@material-ui/icons/Send';
 import { pushMessage } from "../firebase";
-import { InputLabel } from "@material-ui/core";
 
 export function MessageSubmitButton({inputEl, name, text, setText}){
   return (
